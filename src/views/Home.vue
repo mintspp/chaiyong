6bc58a4795dbf9ac6a50da8aea5acbf6bb69cfe1
Suttporn <suttiporn13886@gmail.com>
2021-05-15 16:52:13 +0700

    เพิ่ม

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,7 +33,7 @@
     </div>
 
     <div class="container">
-      <div style="margin-top: 40px;margin-bottom: 40px">
+      <div style="margin-top: 40px; margin-bottom: 40px">
         <b-row>
           <b-col cols="6" lg="3">
             <img alt="Vue logo" width="170px" src="../assets/logo.png" />
@@ -53,56 +53,95 @@
         <b-row>
           <b-col cols="6" lg="4">
             <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
+              img-src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/1.png?alt=media&token=e0d3062d-2311-46f8-9ab6-90130e24d351"
               img-alt="Image"
               img-top
               tag="article"
               style="max-width: 20rem"
               class="mb-2"
             >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
+            <div align="left" >
+                <div class="fonts"><b>กรรไกรตัดผม</b></div>
 
-              <b-button href="#" variant="primary">Go somewhere</b-button>
+                <b-row>
+                  <b-col cols="6"
+                    ><div align="left">
+                      <h6>฿100</h6>
+                    </div>
+                  </b-col>
+                  <b-col cols="6">
+                    <div align="right">
+                      <b-icon
+                        icon="cart4"
+                        style="width: 20px; height: 20px"
+                        variant="success"
+                      ></b-icon>
+                    </div>
+                  </b-col>
+                </b-row>
+              </div>
             </b-card>
           </b-col>
           <b-col cols="6" lg="4">
             <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
+              img-src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/2.png?alt=media&token=f40ffb34-af16-4a16-81d9-780268ccaa76"
               img-alt="Image"
               img-top
               tag="article"
               style="max-width: 20rem"
               class="mb-2"
             >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
+              <div align="left" >
+                <div class="fonts"><b>ชุดกรรไกรตัดผม</b></div>
 
-              <b-button href="#" variant="primary">Go somewhere</b-button>
+                <b-row>
+                  <b-col cols="6"
+                    ><div align="left">
+                      <h6>฿100</h6>
+                    </div>
+                  </b-col>
+                  <b-col cols="6">
+                    <div align="right">
+                      <b-icon
+                        icon="cart4"
+                        style="width: 20px; height: 20px"
+                        variant="success"
+                      ></b-icon>
+                    </div>
+                  </b-col>
+                </b-row>
+              </div>
             </b-card>
           </b-col>
           <b-col cols="6" lg="4">
             <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
+              img-src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/3.png?alt=media&token=eb90bddf-c4dc-4ecf-887e-c27b720a5b62"
               img-alt="Image"
               img-top
               tag="article"
               style="max-width: 20rem"
               class="mb-2"
             >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
+              <div align="left" >
+                <div class="fonts"><b>ไดร์เป่าผม</b></div>
 
-              <b-button href="#" variant="primary">Go somewhere</b-button>
+                <b-row>
+                  <b-col cols="6"
+                    ><div align="left">
+                      <h6>฿100</h6>
+                    </div>
+                  </b-col>
+                  <b-col cols="6">
+                    <div align="right">
+                      <b-icon
+                        icon="cart4"
+                        style="width: 20px; height: 20px"
+                        variant="success"
+                      ></b-icon>
+                    </div>
+                  </b-col>
+                </b-row>
+              </div>
             </b-card>
           </b-col>
         </b-row>
@@ -113,3 +152,6 @@
 
 <script>
 </script>
+
+<style>
+</style>
