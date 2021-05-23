58294573edf81cb0b1b92415b4e7b7deb500189c
Suttporn <suttiporn13886@gmail.com>
2021-05-23 16:38:21 +0700

    เพิ่ม nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+     <div>
+      <b-navbar toggleable="lg" type="dark" style="background-color: #CC6699">
+        <b-navbar-brand href="#">ชัยยงค์การช่าง</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <div style="font-size: 2rem">
+              <b-icon icon="basket2-fill" scale="1" variant="light"></b-icon>
+            </div>
+            <div style="font-size: 2rem">
+              <b-icon icon="bell-fill" scale="1" variant="light"></b-icon>
+            </div>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+</template>
+<script>
+// import axios from "axios";
+export default {
+  name: "Nav",
+  data: () => ({
+  }),
+  mounted() {
+
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,8 @@
 <template>
   <div>
-    <div>
-      <b-navbar toggleable="lg" type="dark" style="background-color: #CC6699">
-        <b-navbar-brand href="#">ชัยยงค์การช่าง</b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            <div style="font-size: 2rem">
-              <b-icon icon="basket2-fill" variant="light"></b-icon>
-            </div>
-            <div style="font-size: 2rem">
-              <b-icon icon="bell-fill" variant="light"></b-icon>
-            </div>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+     <!-- --------------nav------------ -->
+    <Nav />
+    <!-- --------------nav------------ -->
 
     <div>
       <div style="margin-top: 10px">
@@ -38,28 +11,28 @@
             <img
               src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/pink.jpg?alt=media&token=3c7cfbcf-4485-42b4-bf11-e3d8d1fe9114"
               alt="Image"
-              width="300px;"
+              width="100%"
             />
           </b-col>
           <b-col cols="6" lg="3">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/yellow.jpg?alt=media&token=096e4371-8217-478d-99a6-646cfccb5523"
               alt="Image"
-              width="300px;"
+              width="100%"
             />
           </b-col>
           <b-col cols="6" lg="3">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/green.jpg?alt=media&token=42d97550-77b7-4026-bbe2-daec70134236"
               alt="Image"
-              width="300px;"
+              width="100%"
             />
           </b-col>
           <b-col cols="6" lg="3">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/chaiyongimg.appspot.com/o/orange.jpg?alt=media&token=d2004a21-6c3b-403a-be96-274b99630b50"
               alt="Image"
-              width="300px;"
+              width="100%"
             />
           </b-col>
         </b-row>
@@ -483,7 +456,11 @@
 </template>
 
 <script>
+import Nav from "../components/Nav";
 export default {
+   components: {
+    Nav,
+  },
   data: () => ({}),
   created() {},
   mounted() {},
